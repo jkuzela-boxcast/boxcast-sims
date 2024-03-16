75904d3fedc3b2129d1c137c09ed26a1aeb59d97
Jacob K <jgamer9679@gmail.com>
2024-03-16 01:17:10 -0400

    added broadcast details menu

--- src/components/ProBroadcastPreview.vue
+++ src/components/ProBroadcastPreview.vue
@@ -3,39 +3,52 @@ import SvgIcon from "./SvgIcon.vue";
 </script>
 
 <template>
-<div class="h-full w-full bg-[#2c333d]">
-  <div class="grid grid-cols-4 bg-[#353a47]">
-    <div class="col-span-1 flex items-center justify-start pl-2">
-      <SvgIcon @click="$emit('close-clicked')" icon="icon-chevron-down" :width="36"
-        class="cursor-pointer text-bc-white hover:text-bc-blue" />
-    </div>
-    <div class="col-span-2 flex items-center justify-center">
-      <p class="text-xl font-semibold">VIDEO</p>
+  <div class="h-full w-full bg-black/25">
+    <div class="absolute bottom-0 left-0 right-0 top-8 grid grid-cols-1 grid-rows-[17%,auto,auto] bg-[#5cd0d6]">
+      <div class="flex h-10 items-center justify-center">
+        <SvgIcon @click="$emit('close-clicked')" icon="icon-chevron-down" :width="36"
+          class="cursor-pointer text-bc-white hover:text-bc-blue" />
+      </div>
+      <div class="px-4">
+        <p>
+          <span class="rounded-sm bg-red-600 px-2 py-px font-semibold">LIVE</span>
+          0 Viewers
+        </p>
+        <h3 class="text-2xl font-semibold">My First Broadcast</h3>
+        <p>Ending in 5 hrs 41 mins 49 secs</p>
+      </div>
+      <div class="grid grid-cols-4 grid-rows-2 items-start justify-center bg-[#42b4ce] px-2 py-2">
+        <div class="px-2">
+          <p class="text-sm">DURATION</p>
+          <p class="text-sm font-semibold">5 hrs 54 mins</p>
+        </div>
+        <div class="px-2">
+          <p class="text-sm">TYPE</p>
+          <p class="text-sm font-semibold">Private</p>
+        </div>
+        <div class="px-2">
+          <p class="text-sm">VIDEO</p>
+          <p class="text-sm font-semibold">SDI</p>
+        </div>
+        <div class="px-2">
+          <p class="text-sm">AUDIO</p>
+          <p class="text-sm font-semibold">Default</p>
+        </div>
+        <div class="px-2">
+          <p class="text-sm">RESOLUTION</p>
+          <p class="text-sm font-semibold">1080p</p>
+        </div>
+        <div class="px-2">
+          <p class="text-sm">FRAME RATE</p>
+          <p class="text-sm font-semibold">59.94</p>
+        </div>
+        <div class="px-2">
+          <p class="text-sm">BITRATE</p>
+          <p class="text-sm font-semibold">18.30 Mbps</p>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="grid grid-cols-2 grid-rows-1 gap-2 p-4 h-4/6">
-    <div class="grid grid-cols-1 grid-rows-3 gap-1">
-      <div class="row-span-1 flex items-center justify-center bg-[#353a47] px-20 py-2">
-        <h3 class="font-medium">
-          HDMI
-        </h3>
-      </div>
-      <div class="row-span-2 flex items-center justify-center bg-[#4f6272]">
-        <p>Not Connected</p>
-      </div>
-    </div>
-    <div class="grid grid-cols-1 grid-rows-3 gap-1">
-      <div class="row-span-1 flex items-center justify-center bg-[#353a47] px-20 py-2">
-        <h3 class="font-medium">
-          SDI
-        </h3>
-      </div>
-      <div class="row-span-2 flex items-center justify-center bg-[#4f6272]">
-        <p>Not Connected</p>
-      </div>
-    </div>
-  </div>
-</div>
 </template>
 
 <style></style>
--- src/components/SimPro.vue
+++ src/components/SimPro.vue
@@ -73,85 +73,90 @@ watch(selectedNetworkInput, (newValue) => {
 </script>
 
 <template>
-<div class="grid grid-cols-1 grid-rows-[auto,auto]">
-  <div class="flex items-center justify-around p-4">
-    <select v-model="selectedVideoInput" name="VideoInput" id="VideoInput" class="io-selector">
-      <option value="disconnected">Disconnected</option>
-      <option value="hdmi">HDMI</option>
-      <option value="sdi">SDI</option>
-    </select>
-    <select v-model="selectedAudioInput" name="AudioInput" id="AudioInput" class="io-selector">
-      <option value="disconnected">Disconnected</option>
-      <option value="jack">1/4"</option>
-      <option value="xlr">XLR</option>
-    </select>
-    <select v-model="selectedNetworkInput" name="NetworkInput" id="NetworkInput" class="io-selector">
-      <option value="disconnected">Disconnected</option>
-      <option value="wired">Ethernet</option>
-      <option value="wireless">Wi-Fi</option>
-    </select>
-  </div>
-  <div class="relative bg-bc-blue/50">
-    <img src="/src/assets/img/bc_pro_base.png" class="w-[720px]" />
-    <div class="absolute bottom-[106px] left-[142px] right-[124px] top-[133px] overflow-hidden rounded-md bg-bc-black">
-      <div class="grid h-full grid-cols-10 grid-rows-2 gap-1">
-        <button @click="videoMenuOpen = !videoMenuOpen"
-          class="col-span-4 grid grid-cols-1 grid-rows-[auto,75%] bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
-          <div class="flex items-center justify-start pl-1">
-            <SvgIcon icon="icon-video" class="mr-1 mt-[-0.14rem]" />
-            <p>VIDEO</p>
-          </div>
-          <div v-if="VideoStatus.name != undefined" class="-mt-2 flex flex-col items-center justify-center">
-            <p class="text-lg font-medium">{{ VideoStatus.name }}</p>
-            <p>{{ VideoStatus.status }}</p>
-          </div>
-          <div v-else class="-mt-2 flex flex-col items-center justify-center p-10">
-            <p>{{ VideoStatus.status }}</p>
-          </div>
-        </button>
-        <button @click="networkMenuOpen = !networkMenuOpen"
-          class="col-span-4 grid grid-cols-1 grid-rows-[auto,75%] bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
-          <div class="flex items-center justify-start pl-1">
-            <SvgIcon icon="icon-network" :width="16" class="mr-2 mt-[-0.14rem]" />
-            <p>NETWORK</p>
-          </div>
-          <div v-if="NetworkStatus.name != undefined" class="-mt-2 flex flex-col items-center justify-center">
-            <p class="text-lg font-medium">{{ NetworkStatus.name }}</p>
-            <p>{{ NetworkStatus.status }}</p>
-          </div>
-          <div v-else class="-mt-2 flex flex-col items-center justify-center p-4">
-            <p>{{ NetworkStatus.status }}</p>
-          </div>
+  <div class="grid grid-cols-1 grid-rows-[auto,auto]">
+    <div class="flex items-center justify-around p-4">
+      <select v-model="selectedVideoInput" name="VideoInput" id="VideoInput" class="io-selector">
+        <option value="disconnected">Disconnected</option>
+        <option value="hdmi">HDMI</option>
+        <option value="sdi">SDI</option>
+      </select>
+      <select v-model="selectedAudioInput" name="AudioInput" id="AudioInput" class="io-selector">
+        <option value="disconnected">Disconnected</option>
+        <option value="jack">1/4"</option>
+        <option value="xlr">XLR</option>
+      </select>
+      <select v-model="selectedNetworkInput" name="NetworkInput" id="NetworkInput" class="io-selector">
+        <option value="disconnected">Disconnected</option>
+        <option value="wired">Ethernet</option>
+        <option value="wireless">Wi-Fi</option>
+      </select>
+    </div>
+    <div class="relative bg-bc-blue/50">
+      <img src="/src/assets/img/bc_pro_base.png" class="w-[720px]" />
+      <div
+        class="absolute bottom-[106px] left-[142px] right-[124px] top-[133px] overflow-hidden rounded-md bg-bc-black">
+        <div class="grid h-full grid-cols-10 grid-rows-2 gap-1">
+          <button @click="videoMenuOpen = !videoMenuOpen"
+            class="col-span-4 grid grid-cols-1 grid-rows-[auto,75%] bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
+            <div class="flex items-center justify-start pl-1">
+              <SvgIcon icon="icon-video" class="mr-1 mt-[-0.14rem]" />
+              <p>VIDEO</p>
+            </div>
+            <div v-if="VideoStatus.name != undefined" class="-mt-2 flex flex-col items-center justify-center">
+              <p class="text-lg font-medium">{{ VideoStatus.name }}</p>
+              <p>{{ VideoStatus.status }}</p>
+            </div>
+            <div v-else class="-mt-2 flex flex-col items-center justify-center p-10">
+              <p>{{ VideoStatus.status }}</p>
+            </div>
+          </button>
+          <button @click="networkMenuOpen = !networkMenuOpen"
+            class="col-span-4 grid grid-cols-1 grid-rows-[auto,75%] bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
+            <div class="flex items-center justify-start pl-1">
+              <SvgIcon icon="icon-network" :width="16" class="mr-2 mt-[-0.14rem]" />
+              <p>NETWORK</p>
+            </div>
+            <div v-if="NetworkStatus.name != undefined" class="-mt-2 flex flex-col items-center justify-center">
+              <p class="text-lg font-medium">{{ NetworkStatus.name }}</p>
+              <p>{{ NetworkStatus.status }}</p>
+            </div>
+            <div v-else class="-mt-2 flex flex-col items-center justify-center p-4">
+              <p>{{ NetworkStatus.status }}</p>
+            </div>
 
-        </button>
-        <button @click="audioMenuOpen = !audioMenuOpen"
-          class="col-span-2 grid grid-cols-1 grid-rows-[auto,75%] bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
-          <div class="flex items-center justify-center">
-            <SvgIcon icon="icon-audio" :width="16" class="mr-1 mt-[-0.14rem]" />
-            <p>Audio</p>
-          </div>
-          <div class="w-full h-5/6 flex justify-center items-end gap-1 px-[1.65rem]">
-            <div class="bg-[#64dca9] h-1/2 w-full"></div>
-            <div class="bg-[#64dca9] h-1/3 w-full"></div>
-          </div>
-        </button>
-        <button @click="broadcastMenuOpen = !broadcastMenuOpen"
-          class="col-span-6 flex items-start justify-start bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
-          <SvgIcon icon="icon-broadcast" :width="16" class="mr-1 mt-1" />
-          Broadcast
-        </button>
+          </button>
+          <button @click="audioMenuOpen = !audioMenuOpen"
+            class="col-span-2 grid grid-cols-1 grid-rows-[auto,75%] bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
+            <div class="flex items-center justify-center">
+              <SvgIcon icon="icon-audio" :width="16" class="mr-1 mt-[-0.14rem]" />
+              <p>AUDIO</p>
+            </div>
+            <div class="w-full h-5/6 flex justify-center items-end gap-1 px-[1.65rem]">
+              <div class="bg-[#64dca9] h-1/2 w-full"></div>
+              <div class="bg-[#64dca9] h-1/3 w-full"></div>
+            </div>
+          </button>
+          <button @click="broadcastMenuOpen = !broadcastMenuOpen"
+            class="col-span-6 flex items-start justify-start bg-[#353a47] p-1 hover:bg-[#2b2f3b]">
+            <SvgIcon icon="icon-broadcast" :width="16" class="mr-1 mt-1" />
+            Broadcast
+          </button>
+          <button
+            class="col-span-4 flex items-center justify-center font-semibold text-lg bg-[#353a47] px-4 hover:bg-[#2b2f3b]">
+            Preview Broadcast
+          </button>
+        </div>
+        <ProVideoPanel :class="{ 'top-0': videoMenuOpen, 'top-full': !videoMenuOpen }"
+          @close-clicked="videoMenuOpen = false" class="absolute left-0 transition-[top]" />
+        <ProNetworkPanel :class="{ 'top-0': networkMenuOpen, 'top-full': !networkMenuOpen }"
+          @close-clicked="networkMenuOpen = false" class="absolute left-0 transition-[top]" />
+        <ProAudioPanel :class="{ 'top-0': audioMenuOpen, 'top-full': !audioMenuOpen }"
+          @close-clicked="audioMenuOpen = false" class="absolute left-0 transition-[top]" />
+        <ProBroadcastPreview :class="{ 'top-0': broadcastMenuOpen, 'top-full': !broadcastMenuOpen }"
+          @close-clicked="broadcastMenuOpen = false" class="absolute left-0 transition-[top]" />
       </div>
-      <ProVideoPanel :class="{ 'top-0': videoMenuOpen, 'top-full': !videoMenuOpen }"
-        @close-clicked="videoMenuOpen = false" class="absolute left-0 transition-[top]" />
-      <ProNetworkPanel :class="{ 'top-0': networkMenuOpen, 'top-full': !networkMenuOpen }"
-        @close-clicked="networkMenuOpen = false" class="absolute left-0 transition-[top]" />
-      <ProAudioPanel :class="{ 'top-0': audioMenuOpen, 'top-full': !audioMenuOpen }"
-        @close-clicked="audioMenuOpen = false" class="absolute left-0 transition-[top]" />
-      <ProBroadcastPreview :class="{ 'top-0': broadcastMenuOpen, 'top-full': !broadcastMenuOpen }"
-        @close-clicked="broadcastMenuOpen = false" class="absolute left-0 transition-[top]" />
     </div>
   </div>
-</div>
 </template>
 
 <style scoped>
